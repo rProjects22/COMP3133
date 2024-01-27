0f468e93f804a81df42d9cfbeb36dd136e9ca234
rProjects22 <ReadyProjects22@gmail.com>
2024-01-27 13:52:25 -0500

    Lab Exercies

---
+++ canada_stream.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const csvParser = csv();
+
+//Input CSV file path 
+const inputFile = 'input_countries.csv';
+
+//Output TXT file path for Canada 
+const outputFile = 'canada.txt';
+
+//Create read stream from the input CSV file 
+const readStream = fs.createReadStream(inputFile, 'utf-8');
+
+//Create a write stream to the output TXT file 
+const writeStream = fs.createWriteStream(outputFile, { flags: 'w', encoding: 'utf-8' });
+
+
+function filterData(row){
+     return row.country === 'Canada';
+}
+
+// Function to add a title/header line to the output file
+function addTitle() {
+     const title = 'country,year,population\n';
+     writeStream.write(title);
+   }
+
+//delete the file if existing...
+function deleteOutputFile() {
+     if (fs.existsSync(outputFile)) {
+       console.log(`${outputFile} exists. Deleting...`);
+       fs.unlinkSync(outputFile);
+       console.log(`${outputFile} was deleted`);
+     }
+   }
+
+deleteOutputFile();
+   
+addTitle();
+
+readStream
+     .pipe(csvParser)
+     .on('data', (row) => {
+          if (filterData(row)){
+               // Write the filtered data to the output TXT file without additional formatting
+               writeStream.write(`${row.country},${row.year},${row.population}\n`);
+          }
+     })
+     .on('end', () => {
+          // Close the write stream when parsing is complete
+          writeStream.end();
+        })
+
+        .on('error', (err) => {
+          console.error('CSV parser error:', err);
+        })
+
+        .on('finish', () => {
+          console.log('Filtered data for Canada written to', outputFile);
+        });
+
